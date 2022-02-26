25a9072c7dd3caa441226620ce56a71a1edbb993
RafaelHuerta <71308072+hrrRafael2@users.noreply.github.com>
2022-02-25 22:14:02 -0600

    add:test
    Es la primer version , lo ire mejorando , agregandole js y vue

---
+++ styles/hi.py
@@ -0,0 +1,12 @@
+print("Hola mundo)
+
+
+## Falida un formulario con una funcion 
+def formulario():
+    nombre = input("Ingrese su nombre: ")
+    apellido = input("Ingrese su apellido: ")
+    edad = input("Ingrese su edad: ")
+    print("Nombre: ", nombre)
+    print("Apellido: ", apellido)
+    print("Edad: ", edad)
+
